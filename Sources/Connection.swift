026b03b81f8c864cf97178c9c71a5f4b98ae89a6
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-02-25 11:05:21 -0500

    fixed module map

---
+++ Sources/Connection.swift
@@ -0,0 +1,15 @@
+//
+//  Connection.swift
+//  DBus
+//
+//  Created by Alsey Coleman Miller on 2/25/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+import CDBus
+
+/// DBus Connection
+public final class Connection {
+    
+    internal let
+}
