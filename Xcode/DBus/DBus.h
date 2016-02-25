2f1b140d065e8c63aaf2a6bcbb80243b536dd163
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-02-25 09:52:08 -0500

    created Xcode project

---
+++ Xcode/DBus/DBus.h
@@ -0,0 +1,16 @@
+//
+//  DBus.h
+//  DBus
+//
+//  Created by Alsey Coleman Miller on 2/24/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DBus.
+FOUNDATION_EXPORT double DBusVersionNumber;
+
+//! Project version string for DBus.
+FOUNDATION_EXPORT const unsigned char DBusVersionString[];
+
