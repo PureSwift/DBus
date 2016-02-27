40979d3ea590bdda56da14e3b89f8ef98c1abb94
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-02-27 16:01:35 -0500

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "DBus",
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/CDBus.git", majorVersion: 0),
+        .Package(url: "https://github.com/PureSwift/CDBus.git", majorVersion: 1),
     ]
 )
