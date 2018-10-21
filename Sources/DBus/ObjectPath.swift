dfd7ea1865b68bbcb2d8a307fe007d46990cf096
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-10-21 01:31:46 -0500

    Working on ObjectPath

--- Sources/DBus/ObjectPath.swift
+++ Sources/DBus/ObjectPath.swift
@@ -5,39 +5,45 @@
 //  Created by Alsey Coleman Miller on 10/20/18.
 //
 
-public struct DBusObjectPath: RawRepresentable {
-    
-    internal static let length = (min:1, max: 255)
+/// DBus Object Path
+public struct DBusObjectPath {
     
     @_versioned
-    internal let elements: [Element]
+    internal let reference: Reference
+    
+    /// Reference type with value semantics
+    internal init(reference: Reference) {
+        
+        self.reference = reference
+    }
+}
+
+// MARK: - Constants
+
+internal extension DBusObjectPath {
+    
+    static let separator = "/".first!
+}
+
+// MARK: - RawRepresentable
+
+extension DBusObjectPath: RawRepresentable {
     
     public init?(rawValue: String) {
         
-        // The path must begin with an ASCII '/' (integer 47) character,
-        // and must consist of elements separated by slash characters.
-        guard let firstCharacter = rawValue.first,
-            firstCharacter == DBusObjectPath.separator
+        guard let reference = Reference(string: rawValue)
             else { return nil }
         
-        let elements = rawValue.split(separator: DBusObjectPath.separator,
-                                      maxSplits: .max,
-                                      omittingEmptySubsequences: true)
-            .compactMap {  }
-        
-        
+        self.init(reference: reference)
     }
     
     public var rawValue: String {
         
-        return path.reduce("", { $0 + "/" + $1.rawValue })
+        get { return reference.string }
     }
 }
 
-private extension DBusObjectPath {
-    
-    static let separator = "/".first!
-}
+// MARK: - CustomStringConvertible
 
 extension DBusObjectPath: CustomStringConvertible {
     
@@ -64,10 +70,83 @@ public extension DBusObjectPath {
     }
 }
 
+private extension DBusObjectPath.Element {
+    
+    static let length = (min: 1, max: 255)
+}
+
 extension DBusObjectPath.Element: CustomStringConvertible {
     
     public var description: String {
         
         return rawValue
+    }
+}
+
+// MARK: - Reference Backed Value Type
+
+internal extension DBusObjectPath {
+    
+    /// Internal cache
+    final class Reference {
+        
+        /// initialize with the elements
+        internal init(elements: [Element]) {
+            
+            self.elements = elements
+        }
+        
+        /// Initialize with a string.
+        internal convenience init?(string: String) {
+            
+            // The path must begin with an ASCII '/' (integer 47) character,
+            // and must consist of elements separated by slash characters.
+            guard let firstCharacter = string.first,
+                firstCharacter == DBusObjectPath.separator
+                else { return nil }
+            
+            let pathStrings = string.split(separator: DBusObjectPath.separator,
+                                           maxSplits: .max,
+                                           omittingEmptySubsequences: true)
+            
+            var elements = [Element]()
+            elements.reserveCapacity(pathStrings.count) // pre-allocate buffer
+            
+            for elementString in pathStrings {
+                
+                guard let element = Element(rawValue: String(elementString))
+                    else { return nil }
+                
+                elements.append(element)
+            }
+            
+            self.init(elements: elements)
+            self.stringCache = string // store parsed string
+        }
+        
+        /// Parsed elements. Always initialized to this value.
+        internal let elements: [Element]
+        
+        /// Cached String value.
+        private var stringCache: String?
+        
+        /// lazily initialized string value
+        internal var string: String {
+            
+            guard let cache = stringCache else {
+                
+                // lazily initialize
+                let separator = String(DBusObjectPath.separator)
+                let stringValue = elements.isEmpty ? separator :
+                    elements.reduce("", { $0 + separator + $1.rawValue })
+                
+                // cache value
+                stringCache = stringValue
+                
+                return stringValue
+            }
+            
+            return cache
+        }
     }
 }
