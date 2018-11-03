b2a95c5cb598193dcf6f11d945383b3aa27352d0
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-11-03 12:47:34 -0500

    Working on Message

--- Sources/DBus/Message.swift
+++ Sources/DBus/Message.swift
@@ -85,8 +85,7 @@ public final class DBusMessage {
     public func append(_ argument: DBusMessageArgument) throws {
         
         var iterator = DBusMessageIter(message: self)
-        
-        
+        try iterator.append(argument: argument)
     }
     
     // MARK: - Properties
@@ -102,28 +101,6 @@ public final class DBusMessage {
         return type
     }
     
-    /// The message's arguments.
-    /*
-    public var arguments: [DBusMessageArgument] {
-        
-        get {
-            
-            var iterator = DBusMessageIter()
-            
-            guard dbus_message_iter_init(internalPointer, &iterator)
-                else { return [] }
-            
-            //while let currentType = dbus_message_iter_get_arg_type(&iterator) where current_type != DBUS_TYPE_INVALID { }
-            
-            fatalError("Not implemented")
-        }
-        
-        set { append(newValue) }
-    }
-    */
-    
-    
-    
     /// Checks whether a message contains Unix file descriptors.
     public var containsFileDescriptors: Bool {
         
@@ -138,7 +115,7 @@ public final class DBusMessage {
     /// - Note: All messages received on a connection will have a serial provided by the remote application.
     ///
     /// For messages you're sending, `DBusConnection.send()` will assign a serial and return it to you.
-    public var serial: dbus_uint32_t {
+    public var serial: UInt32 {
         
         get { return dbus_message_get_serial(internalPointer) }
         
@@ -146,15 +123,16 @@ public final class DBusMessage {
     }
     
     /// The reply serial of a message (the serial of the message this is a reply to).
-    public var replySerial: dbus_uint32_t {
+    public var replySerial: UInt32 {
         
-        get { return dbus_message_get_reply_serial(internalPointer) }
+        return dbus_message_get_reply_serial(internalPointer)
+    }
+    
+    /// Sets the reply serial of a message (the serial of the message this is a reply to).
+    public func setReplySerial(_ newValue: UInt32) throws {
         
-        set {
-            
-            guard Bool(dbus_message_set_reply_serial(internalPointer, newValue))
-                else { fatalError("Out of memory!") }
-        }
+        guard Bool(dbus_message_set_reply_serial(internalPointer, newValue))
+            else { throw DBusError.messageSetValueOutOfMemory }
     }
     
     /// Flag indicating that the caller of the method is prepared to wait for interactive authorization to take place 
@@ -203,31 +181,58 @@ public final class DBusMessage {
     /// The destination name must contain only valid characters as defined in the D-Bus specification.
     public var destination: String? {
         
-        get { return getString(dbus_message_get_destination) }
+        return getString(dbus_message_get_destination)
+    }
+    
+    /// Sets the message's destination.
+    public func setDestination(_ newValue: String?) throws {
         
-        set { setString(dbus_message_set_destination, newValue) }
+        try setString(dbus_message_set_destination, newValue)
     }
     
     /// The name of the error (for `Error` message type).
     ///
     /// The name is fully-qualified (namespaced). 
     /// The error name must contain only valid characters as defined in the D-Bus specification.
-    public var errorName: String? {
+    public var errorName: DBusError.Name? {
         
-        get { return getString(dbus_message_get_error_name) }
+        guard let string = getString(dbus_message_get_error_name)
+            else { return nil }
         
-        set { setString(dbus_message_set_error_name, newValue) }
+        guard let name = DBusError.Name(rawValue: string)
+            else { fatalError("Invalid error name \(string)") }
+        
+        return name
+    }
+    
+    /// Sets the name of the error (DBUS_MESSAGE_TYPE_ERROR).
+    /// The name is fully-qualified (namespaced).
+    /// The error name must contain only valid characters as defined in the D-Bus specification.
+    public func setErrorName(_ newValue: DBusError.Name?) throws {
+        
+        try setString(dbus_message_set_error_name, newValue?.rawValue)
     }
     
     /// The interface this message is being sent to (for method call type) 
     /// or the interface a signal is being emitted from (for signal call type).
     ///
     /// The interface name must contain only valid characters as defined in the D-Bus specification.
-    public var interface: String? {
+    public var interface: DBusInterface? {
         
-        get { return getString(dbus_message_get_interface) }
+        guard let string = getString(dbus_message_get_interface)
+            else { return nil }
         
-        set { setString(dbus_message_set_interface, newValue) }
+        guard let interface = DBusInterface(rawValue: string)
+            else { fatalError("Invalid interface \(string)") }
+        
+        return interface
+    }
+    
+    /// Sets the interface this message is being sent to (for DBUS_MESSAGE_TYPE_METHOD_CALL)
+    /// or the interface a signal is being emitted from (for DBUS_MESSAGE_TYPE_SIGNAL).
+    public func setInterface(_ newValue: DBusInterface?) throws {
+        
+        try setString(dbus_message_set_interface, newValue?.rawValue)
     }
     
     /// The object path this message is being sent to (for method call type)
@@ -276,87 +281,19 @@ public final class DBusMessage {
         return String(cString: cString)
     }
     
-    private func setString(_ function: (OpaquePointer?, UnsafePointer<Int8>?) -> (dbus_bool_t), _ newValue: String?) {
+    private func setString(_ function: (OpaquePointer?, UnsafePointer<Int8>?) -> (dbus_bool_t), _ newValue: String?) throws {
         
         if let newValue = newValue {
             
             guard Bool(newValue.withCString({ function(internalPointer, $0) }))
-                else { fatalError("Out of memory! Could not set \"\(newValue)\" for function \(function)") }
+                else { throw DBusError.messageSetValueOutOfMemory }
             
         } else {
             
             guard Bool(function(internalPointer, nil))
-                else { fatalError("Out of memory! Could not set <Nil String> for function \(function)") }
+                else { throw DBusError.messageSetValueOutOfMemory }
         }
     }
-    
-    /*
-    private func append(arguments: [DBusMessageArgument]) {
-        
-        var iterator = DBusMessageIter()
-        
-        dbus_message_iter_init_append(internalPointer, &iterator)
-        
-        for argument in arguments {
-            
-            switch argument {
-                
-            case var .Byte(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.Byte.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case let .Boolean(boolean):
-                
-                var value = dbus_bool_t(boolean)
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.Boolean.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case var .Int16(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.Int16.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case var .UInt16(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.UInt16.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case var .Int32(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.Int32.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case var .UInt32(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.UInt32.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case var .Int64(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.Int64.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case var .UInt64(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.UInt64.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case var .Double(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.Double.integerValue, &value)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            case let .String(value):
-                
-                guard dbus_message_iter_append_basic(&iterator, DBusType.String.integerValue, &string.0)
-                    else { fatalError("Out of memory! could not append \(argument)") }
-                
-            default: fatalError("Not implemented appending: \(argument)")
-            }
-        }
-    }*/
 }
 
 // MARK: - Copying
@@ -408,3 +345,13 @@ public extension DBusMessage {
     }
 }
 
+private extension DBusError {
+    
+    // Could not modify message due to lack of memory.
+    static var messageSetValueOutOfMemory: DBusError {
+        
+        // If this fails due to lack of memory, the message is hosed and you have to start over building the whole message.
+        // FALSE if not enough memory
+        return DBusError(name: .noMemory, message: "Could not modify message due to lack of memory.")
+    }
+}
---
+++ Tests/DBusTests/MessageTests.swift
@@ -0,0 +1,22 @@
+//
+//  MessageTests.swift
+//  DBusTests
+//
+//  Created by Alsey Coleman Miller on 11/3/18.
+//
+
+import Foundation
+import XCTest
+@testable import DBus
+
+final class MessageTests: XCTestCase {
+    
+    static let allTests = [
+        (testInvalidArguments, "testInvalidArguments")
+    ]
+    
+    func testInvalidArguments() {
+        
+        
+    }
+}
