bdc8e4dd20f85f21726f4c54ca838e283ee8cbcd
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-11-03 19:07:04 -0500

    Updated unit tests

--- Tests/DBusTests/MessageTests.swift
+++ Tests/DBusTests/MessageTests.swift
@@ -12,7 +12,8 @@ import XCTest
 final class MessageTests: XCTestCase {
     
     static let allTests = [
-        (testBasicValueArguments, "testBasicValueArguments")
+        (testBasicValueArguments, "testBasicValueArguments"),
+        (testArrayArguments, "testArrayArguments")
     ]
     
     func testBasicValueArguments() {
@@ -44,17 +45,30 @@ final class MessageTests: XCTestCase {
     func testArrayArguments() {
         
         let arguments: [DBusMessageArgument] = [
-            .array(DBusMessageArgument.Array(type: .int32)),
-            .array(DBusMessageArgument.Array(type: .int32, [])!),
+            .array(DBusMessageArgument.Array(type: .int16, [.int16(1), .int16(2), .int16(3)])!),
+            .array(DBusMessageArgument.Array(type: .int16)),
             .array(DBusMessageArgument.Array(type: .int32, [.int32(1), .int32(2), .int32(3)])!),
-            .array(DBusMessageArgument.Array(type: .array(.int32)))
+            .array(DBusMessageArgument.Array(type: .int32)),
+            .array(DBusMessageArgument.Array(type: .string, [.string("1"), .string("2"), .string("3")])!),
+            .array(DBusMessageArgument.Array(type: .string)),
+            .array(DBusMessageArgument.Array(type: .objectPath, [
+                .objectPath(DBusObjectPath("/com/example/bus1")),
+                .objectPath(DBusObjectPath("/com/example/bus2")),
+                .objectPath(DBusObjectPath("/com/example/bus3"))
+                ])!),
+            .array(DBusMessageArgument.Array(type: .objectPath)),
+            .array(DBusMessageArgument.Array(type: .array(.string), [
+                .array(DBusMessageArgument.Array(type: .string, [.string("A1"), .string("A2"), .string("A3")])!),
+                .array(DBusMessageArgument.Array(type: .string, [.string("B1"), .string("B2"), .string("B3")])!),
+                .array(DBusMessageArgument.Array(type: .string, [.string("C1"), .string("C2"), .string("C3")])!)
+                ])!)
         ]
         
         do {
             
             let message = try DBusMessage(type: .methodCall)
             try message.append(contentsOf: arguments)
-            //XCTAssertEqual(Array(message), arguments, "Could not iterate message")
+            XCTAssertEqual(Array(message), arguments, "Could not iterate message")
         }
             
         catch { XCTFail("\(error)") }
